e95a1699e7aef2c547678e1d921edba744dadd51
anoncoder555 <alicegmurray@gmail.com>
2024-11-16 10:51:24 +0000

    Commiting three R scripts (linear models and plots)
    git rebase --continue

--- plot_data_and_model.R
+++ plot_data_and_model.R
@@ -1,6 +1,6 @@
 #Script to plot data and model
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("experiment.csv")
 
 logistic_fun <- function(t) {
   
@@ -8,20 +8,28 @@ logistic_fun <- function(t) {
   
   return(N)
   
-}
+} # sets up paramters of linear model
 
-N0 <- ??? #
-  
-r <- ??? #
-  
-K <- ??? #
+N0 <- exp(6.8941709) # Intecept / ln(N0) from linear model so to get N0 we use exp()
 
-ggplot(aes(???,???), data = growth_data) +
+r <- 0.0100086
+
+K <- 5.979e+10 # Values extracted from the fit_linear_model.R code 
+
+ggplot(aes(t, N), data = growth_data) +
   
   geom_function(fun=logistic_fun, colour="red") +
   
-  geom_point()
+  geom_point() +
+  
+  scale_y_continuous(trans='log10')
 
-  #scale_y_continuous(trans='log10')
+## Commit and push changes to github ##
+
+sink(file = "package-versions.txt")
+sessionInfo()
+sink()
+
+
 
 
